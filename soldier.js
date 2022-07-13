7412acd0030d0535e74a975d3687286c077e1781
MovsarTS <mvs451@mail.ru>
2022-07-13 14:28:30 +0300

    Отформатировал файл

--- soldier.js
+++ soldier.js
@@ -11,30 +11,30 @@ const soldier = {
   // Действия персонажа:
   shoot: function () {
     if (this.weapon.cartridges === 0) {
-      console.log("Обойма пуста. Перезарядись!")
+      console.log("Обойма пуста. Перезарядись!");
     } else {
       this.weapon.cartridges = this.weapon.cartridges - 1;
-      console.log("*Бах-Бах* |Звуки выстрела|")
+      console.log("*Бах-Бах* |Звуки выстрела|");
     }
   },
   recharge: function () {
     if (this.supplies === 0) {
-      console.log("Не осталось припасов")
+      console.log("Не осталось припасов");
     } else {
       soldier.weapon.cartridges = 30;
-      console.log("Перезарядка... |Теперь в магазине 30 патронов|")
+      console.log("Перезарядка... |Теперь в магазине 30 патронов|");
     }
   },
   wound: function () {
     if (this.health <= 1) {
       this.health = this.health - 1;
-      console.log("Ты проиграл |Игра окончена|")
+      console.log("Ты проиграл |Игра окончена|");
     } else {
       this.health = this.health - 1;
-      console.log(`Тебя ранили (-1) |Текущее здоровье: ${this.health}|`)
-  }
-  }
-}
+      console.log(`Тебя ранили (-1) |Текущее здоровье: ${this.health}|`);
+    }
+  },
+};
 
 /* Возможности персонажа */
 // Стрелять:
