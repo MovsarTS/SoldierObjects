8bbf10df8affee311df33bd31a6e768a300682a3
MovsarTS <mvs451@mail.ru>
2022-07-13 14:27:46 +0300

    Сделал методы

--- soldier.js
+++ soldier.js
@@ -1,7 +1,7 @@
 // Характеристики персонажа:
 const soldier = {
   name: "Арнольд",
-  health: 10,
+  health: 0,
   weapon: {
     model: ["Автомат М4"],
     cartridges: 30,
@@ -9,10 +9,32 @@ const soldier = {
   supplies: 3,
 
   // Действия персонажа:
-  shoot: function () {},
-  recharge: function () {},
-  wound: function () {},
-};
+  shoot: function () {
+    if (this.weapon.cartridges === 0) {
+      console.log("Обойма пуста. Перезарядись!")
+    } else {
+      this.weapon.cartridges = this.weapon.cartridges - 1;
+      console.log("*Бах-Бах* |Звуки выстрела|")
+    }
+  },
+  recharge: function () {
+    if (this.supplies === 0) {
+      console.log("Не осталось припасов")
+    } else {
+      soldier.weapon.cartridges = 30;
+      console.log("Перезарядка... |Теперь в магазине 30 патронов|")
+    }
+  },
+  wound: function () {
+    if (this.health <= 1) {
+      this.health = this.health - 1;
+      console.log("Ты проиграл |Игра окончена|")
+    } else {
+      this.health = this.health - 1;
+      console.log(`Тебя ранили (-1) |Текущее здоровье: ${this.health}|`)
+  }
+  }
+}
 
 /* Возможности персонажа */
 // Стрелять:
